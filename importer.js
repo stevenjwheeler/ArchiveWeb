d4728bffe939239d727110388856c106775703d1
robotprobot <steven@stevenwheeler.co.uk>
2023-01-11 19:04:37 +0000

    added importer file

---
+++ importer.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+module.exports = {
+    async runImport(archiveSourceURL, cookies, archivePath, archiveDatabase) {
+        // run gallery-dl to download the archive
+        
+
+    }
+}
