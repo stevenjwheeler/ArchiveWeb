ab2e95c4feb25da1fc461ed9ad500e087ee01163
robotprobot <steven@stevenwheeler.co.uk>
2023-01-15 15:47:09 +0000

    added error logging

--- dependencyInstaller-Linux.js
+++ dependencyInstaller-Linux.js
@@ -4,27 +4,31 @@ module.exports = {
   async installDependencies() {
     const { spawn } = require("child_process");
 
-    // install python3 if it is not installed
-    if (!fs.existsSync("/usr/bin/python3")) {
-      console.log("Installing python3...");
-      const pythonInstall = spawn("sudo", ["apt", "install", "python3", "-y"]);
-      pythonInstall.stdout.on("data", (data) => {
-        console.log(data.toString());
-      });
-      pythonInstall.stderr.on("data", (data) => {
-        console.log(data.toString());
-      });
-      pythonInstall.on("close", (code) => {
-        if (code !== 0) {
-          console.log("Python3 install failed");
-          process.exit(1);
-        } else {
-          console.log("Python3 install successful");
-        }
-      });
+    try {
+      // install python3 if it is not installed
+      if (!fs.existsSync("/usr/bin/python3")) {
+        console.log("Installing python3...");
+        const pythonInstall = spawn("sudo", ["apt", "install", "python3", "-y"]);
+        pythonInstall.stdout.on("data", (data) => {
+          console.log(data.toString());
+        });
+        pythonInstall.stderr.on("data", (data) => {
+          console.log(data.toString());
+        });
+        pythonInstall.on("close", (code) => {
+          if (code !== 0) {
+            console.log("Python3 install failed");
+            process.exit(1);
+          } else {
+            console.log("Python3 install successful");
+          }
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
 
-    // install python3-pip if it is not installed
+   /*  // install python3-pip if it is not installed
     if (!fs.existsSync("/usr/bin/pip3")) {
       console.log("Installing python3-pip...");
       const pipInstall = spawn("sudo", ["apt", "install", "python3-pip", "-y"]);
@@ -68,25 +72,30 @@ module.exports = {
         }
       });
     }
+ */
 
     // install gallery-dl if it is not installed
-    if (!fs.existsSync("/usr/local/bin/gallery-dl")) {
-      console.log("Installing gallery-dl...");
-      const galleryDlInstall = spawn("sudo", ["pip3", "install", "gallery-dl"]);
-      galleryDlInstall.stdout.on("data", (data) => {
-        console.log(data.toString());
-      });
-      galleryDlInstall.stderr.on("data", (data) => {
-        console.log(data.toString());
-      });
-      galleryDlInstall.on("close", (code) => {
-        if (code !== 0) {
-          console.log("Gallery-dl install failed");
-          process.exit(1);
-        } else {
-          console.log("Gallery-dl install successful");
-        }
-      });
+    try {
+      if (!fs.existsSync("/usr/local/bin/gallery-dl")) {
+        console.log("Installing gallery-dl...");
+        const galleryDlInstall = spawn("sudo", ["pip3", "install", "gallery-dl"]);
+        galleryDlInstall.stdout.on("data", (data) => {
+          console.log(data.toString());
+        });
+        galleryDlInstall.stderr.on("data", (data) => {
+          console.log(data.toString());
+        });
+        galleryDlInstall.on("close", (code) => {
+          if (code !== 0) {
+            console.log("Gallery-dl install failed");
+            process.exit(1);
+          } else {
+            console.log("Gallery-dl install successful");
+          }
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
   },
 };
